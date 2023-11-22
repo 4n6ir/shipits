9d2b71fa5c39b2d98ec4dbd90302abaf88139eab
John Lukach <john@lukach.net>
2023-11-22 02:20:17 +0000

    2.110.1 (build 0d37f0d)

---
+++ app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from shipits.shipits_stack import ShipitsStack
+
+app = cdk.App()
+
+ShipitsStack(
+    app, 'ShipitsStackUSE1',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+ShipitsStack(
+    app, 'ShipitsStackUSE2',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-2'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+ShipitsStack(
+    app, 'ShipitsStackUSW2',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-west-2'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+cdk.Tags.of(app).add('Alias','ALL')
+cdk.Tags.of(app).add('GitHub','https://github.com/jblukach/shipits.git')
+
+app.synth()
